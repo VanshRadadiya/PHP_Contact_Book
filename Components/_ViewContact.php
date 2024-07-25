6c509b03a84fc83bce80f81c1cf7374abf159ff5
VanshRadadiya <vanshradadiya95@gmail.com>
2024-07-25 10:52:34 +0530

    contact book

--- Components/_ViewContact.php
+++ Components/_ViewContact.php
@@ -49,7 +49,7 @@ $result1 = mysqli_query($conn, $search_select);
     <title>View Contact</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+ 
 </head>
 
 <body>
